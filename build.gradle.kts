c92704b2a18a93ede79a40c7403cb4f2146eff38
Rodolpho Couto <rodolpho.sbaraglini@gmail.com>
2020-01-12 16:38:27 -0300

    Configure ktor and kotlinx serialization dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,18 @@
 plugins {
     kotlin("multiplatform") version "1.3.60"
+    kotlin("plugin.serialization") version "1.3.60"
     id("org.jlleitschuh.gradle.ktlint") version "9.1.1"
 }
 
 repositories {
     mavenCentral()
+    maven { setUrl("https://kotlin.bintray.com/kotlinx") }
+    maven { setUrl("https://dl.bintray.com/kotlin/ktor") }
+    maven { setUrl("https://dl.bintray.com/jetbrains/kotlin-native-dependencies") }
 }
+
+val ktorVersion = "1.3.0-beta-2"
+val serializationVersion = "0.14.0-1.3.60-eap-76"
 
 kotlin {
     linuxX64("native") {
@@ -21,6 +28,9 @@ kotlin {
         commonMain {
             dependencies {
                 implementation(kotlin("stdlib-common"))
+                implementation("io.ktor:ktor-client-core:$ktorVersion")
+                implementation("io.ktor:ktor-client-serialization:$ktorVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime-common:$serializationVersion")
             }
         }
 
@@ -28,6 +38,15 @@ kotlin {
             dependencies {
                 implementation(kotlin("test-common"))
                 implementation(kotlin("test-annotations-common"))
+            }
+        }
+
+        val nativeMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-curl:$ktorVersion")
+                implementation("io.ktor:ktor-client-core-native:$ktorVersion")
+                implementation("io.ktor:ktor-client-serialization-native:$ktorVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime-native:$serializationVersion")
             }
         }
     }
